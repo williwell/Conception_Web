32a218ee859680d042106f09e4409bb4067371cc
William-Arthur Cinq-Mars <will_arth53@hotmail.com>
2021-02-02 13:25:07 -0500

    Evenement accueil functionnel reste juste a changer le lien

--- JS/Script.js
+++ JS/Script.js
@@ -30,14 +30,15 @@ function printCards(list) {
 function putEvent(list) {
 	console.log("downloading book review: success");
     for (var i = 0; i < list.length; i++) {
-					$("#Event").append("<div class='col-md-3'>"+
-					"<h5>"+list[i][1]+"</h5>"+
-					"<h5>"+list[i][2]+"</h5>"+
+					$("#Event").append("<div id='EventClick' class='col-md-3'>"+
+					"<h5 style='cursor: pointer;'>"+list[i][1]+"</h5>"+
+					"<h5 style='cursor: pointer;'>"+list[i][2]+"</h5>"+
 				  "</div>"
 	  			);
 				
 	}
 }
+
 
 function putActualite(list) {
 	console.log("downloading book review: success");
