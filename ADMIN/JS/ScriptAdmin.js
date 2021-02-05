429ca989666b6a63b52469ae431c0fe27cdd8d6f
Alexandra Sirois <asirois9191@gmail.com>
2021-02-05 14:22:27 -0500

    ajout form enseignant

--- ADMIN/JS/ScriptAdmin.js
+++ ADMIN/JS/ScriptAdmin.js
@@ -1,7 +1,10 @@
 function printCardsAdmin(list) {
 	console.log("téléchargement de la card des enseignant: success");
-	
+	var nbrGestion = 0;
     for (var i = 0; i < list.length; i++) {
+		var id="#"+list[i][0];
+		var matricle= list[i][0];
+		var idClose = list[i][0]+"close";
 					$("#Teacher").append("<div id='"+list[i][0]+"' class='col-xl justify-content-center align-items-center' style='display:flex;'>"+
 					"<div class='card' data-effect='zoom'>"+
 					"<button class='card__save  js-save' type='button'>"+
@@ -23,10 +26,66 @@ function printCardsAdmin(list) {
 				  "</div>"+
 				"</div>"
 				  );
-				  var id="#"+list[i][0];
+				
 				  $(id).click(function() {
-					  location.replace("Menu.html");
+					if(nbrGestion == 0){
+						nbrGestion = 1;
+						$("#GestionEnseignant").append(
+						"<div class='teacherAdminCard justify-content-evenly'>"+
+								"<div class='row justify-content-end'>"+
+									"<div class='col align-self-end'>"+
+										"<i  class='material-icons closeIcon'>close</i>"+
+									"</div>"+  
+								"</div>"+ 
+								
+								
+							"<div class='row justify-content-evenly'>"+
+								"<div class='col justify-content-center align-items-center'>"+
+									"<img src='../../RESSOURCES/PHOTO_ENSEIGNANT/"+matricle+".png' alt='author picture' width='100%' height='70%'>"+
+									"<p class='horizontal' ><span class='text'>Changer la photo</span></p>"+
+								"</div>"+
+
+								"<div class='col'>"+
+									"<form action='' method='post' enctype='text/plain'>"+
+										"<div class='col'>"+
+											"<label for='Prenom'>Prénom</label><br>"+
+											"<input type='text' class='form-control' id=Prenom name=Prenom>"+
+										"</div>"+
+										
+										"<div class='col'><br>"+
+											"<label for='Nom'>Nom</label><br>"+
+											"<input type='text' class='form-control' id=Nom name=Nom>"+
+										"</div>"+
+										
+										"<div class='col'><br>"+
+											"<label for='Adresse'>Courriel</label>"+
+											"<input type='email' class='form-control' id=Adresse name=Adresse>"+
+										"</div><br>"+
+										
+										"<div class='col'>"+
+											"<label for='Message'>Message</label><br>"+
+											"<textarea type='text' class='form-control' id=Message name=Message rows='7'></textarea>"+
+										"</div><br>"+
+										
+										"<div class='row justify-content-evenly'>"+
+											"<p class='horizontal'><span class='text'>Sauvegarder</span></p>"+
+											"<p class='horizontal' ><span class='text'>Supprimer le profil</span></p>"+
+											
+										"</div>"+ 
+									"</form>"+ 
+								"</div>"+
+								
+							"</div>"+    
+    
+							"</div>"
+							);
+							$(".closeIcon").click(function(){
+								nbrGestion = 0;
+								$("#GestionEnseignant").empty();
+							});
+					}
 				  });
+			
     }
    
 
