3321379124c88ad5fdc5712abe7f854050cd1b56
Alexandra Sirois <asirois9191@gmail.com>
2021-03-12 07:54:26 -0500

    changer les liens pour qu'il soit dynamique

--- ADMIN/JS/ScriptAdmin.js
+++ ADMIN/JS/ScriptAdmin.js
@@ -35,8 +35,99 @@ function printCardsAdmin(list) {
 
     }
    
-
 }
+
+function printCards(list) {
+	console.log("téléchargement de la card des enseignant: success");
+	
+    for (var i = 0; i < list.length; i++) {
+					$("#Teacher").append("<div class='col-xl justify-content-center align-items-center' style='display:flex;'>"+
+					"<div class='card' data-effect='zoom'>"+
+					"<button class='card__save  js-save' type='button'>"+
+					  "<i class='fa  fa-bookmark'></i>"+
+					"</button>"+
+                    "<figure class='card__image'>"+
+					  "<img src='../RESSOURCES/PHOTO_ENSEIGNANT/background.png' alt='book cover'>"+
+					"</figure>"+
+					"<div class='card__header'>"+
+					"<figure class='card__profile'>"+
+						"<img src='../RESSOURCES/PHOTO_ENSEIGNANT/"+list[i][0]+".png' alt='author picture' width='25px' height='25px'>"+
+					  "</figure>"+
+					"</div>"+
+					"<div class='card__body'>"+
+					  "<h3 class='card__name'>"+list[i][1]+' '+list[i][2]+"</h3>"+
+					  "<p class='card__job'>"+list[i][3]+"</p>"+
+                      "<p class='card__bio'>"+list[i][4]+"<br>"+list[i][5]+" poste "+list[i][6]+"</p>"+
+					"</div>"+
+				  "</div>"+
+				"</div>"
+	  			);
+				
+	}
+}
+
+
+function printProg(list){
+	console.log("téléchargement du lien: success");
+	
+    for (var i = 0; i < list.length; i++) {
+		if(list[i][1]=="Programmation"){
+					$("#Link").append(
+						"<div class='containerLink center'>"+
+						"<div class='card'>"+
+						  "<h2>"+list[i][4]+"</h2>"+
+						  "<hr/>"+
+						  "<p>"+list[i][2]+"</p>"+
+						  "<div id='btnLien' onclick='changerLocation(\""+list[i][3]+"\")'>lien</div>"+
+						"</div>"+
+					  "</div>"
+				  );
+			}
+	}
+}
+
+function changerLocation(liens){
+	window.location.replace(liens);
+}
+
+function printInfo(list){
+	console.log("téléchargement du lien: success");
+	
+    for (var i = 0; i < list.length; i++) {
+		if(list[i][1]=="Informatique"){
+			$("#Link").append(
+				"<div class='containerLink center'>"+
+				"<div class='card'>"+
+				  "<h2>"+list[i][4]+"</h2>"+
+				  "<hr/>"+
+				  "<p>"+list[i][2]+"</p>"+
+				  "<div id='btnLien' onclick='changerLocation(\""+list[i][3]+"\")'>lien</div>"+
+				"</div>"+
+			  "</div>"
+		  );
+			}	
+	}
+}
+
+function printAnnonces(list){
+	console.log("téléchargement du lien: success");
+	
+    for (var i = 0; i < list.length; i++) {
+		if(list[i][1]=="Annonce"){
+			$("#Link").append(
+				"<div class='containerLink center'>"+
+				"<div class='card'>"+
+				  "<h2>"+list[i][4]+"</h2>"+
+				  "<hr/>"+
+				  "<p>"+list[i][2]+"</p>"+
+				  "<div id='btnLien' onclick='changerLocation(\""+list[i][3]+"\")'>lien</div>"+
+				"</div>"+
+			  "</div>"
+		  );
+			}	
+	}
+}
+
 
 function putEventDetail(list) {
 	console.log("downloading event detail");
@@ -325,15 +416,15 @@ function sauvegarderEnseignant(matricule){
 	var telephone= $("#Telephone").val();
 	var poste=$("#Poste").val();
 
-		if(prenom=="" || prenom==null || !(/^[a-zA-Z]+$/.test(prenom)) || prenom.length>50 ){
+		if(prenom=="" || prenom==null || !(/^[A-Za-z\s]+$/.test(prenom)) || prenom.length>50 ){
 			alert("Le champs prénom est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 50 caractères");
 		}
 		else{
-			if(nom=="" || nom==null || !(/^[a-zA-Z]+$/.test(nom)) || nom.length>50){
+			if(nom=="" || nom==null || !(/^[A-Za-z\s]+$/.test(nom)) || nom.length>50){
 				alert("Le champs nom est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 50 caractères");
 			}
 			else{
-				if(typeEmployer=="" || typeEmployer==null || !(/^[a-zA-Z]+$/.test(typeEmployer))|| typeEmployer.length>200){
+				if(typeEmployer=="" || typeEmployer==null || !(/^[A-Za-z\s]+$/.test(typeEmployer))|| typeEmployer.length>200){
 					alert("Le champs type d'employé est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 200 caractères");
 				}
 				else{
@@ -549,15 +640,15 @@ function ajouterEnseignant(){
 		alert("Le champs matricule est obligatoire, vérifiez que le matricule entré suit le format suivant: 1234567");
 	}
 	else{
-		if(prenom=="" || prenom==null || !(/^[a-zA-Z]+$/.test(prenom)) || prenom.length>50 ){
+		if(prenom=="" || prenom==null || !(/^[A-Za-z\s]+$/.test(prenom)) || prenom.length>50 ){
 			alert("Le champs prénom est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 50 caractères");
 		}
 		else{
-			if(nom=="" || nom==null || !(/^[a-zA-Z]+$/.test(nom)) || nom.length>50){
+			if(nom=="" || nom==null || !(/^[A-Za-z\s]+$/.test(nom)) || nom.length>50){
 				alert("Le champs nom est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 50 caractères");
 			}
 			else{
-				if(typeEmployer=="" || typeEmployer==null || !(/^[a-zA-Z]+$/.test(typeEmployer))|| typeEmployer.length>200){
+				if(typeEmployer=="" || typeEmployer==null || !(/^[A-Za-z\s]+$/.test(typeEmployer))|| typeEmployer.length>200){
 					alert("Le champs type d'employé est obligatoire, il doit être composé seulement de lettres. Aussi, la longeur maximale pour ce champ est de 200 caractères");
 				}
 				else{
